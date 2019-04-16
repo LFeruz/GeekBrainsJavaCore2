7132901d783bf8ebc09019a6e28a98a1f85c7575
Nikolay.Pluzhnikov <Nikolay.Pluzhnikov@nexign-systems.com>
2019-04-16 18:26:33 +0700

    Исправление коммита

--- src/ru/geekbrains/lesson5/homework/CalcRunnable.java
+++ src/ru/geekbrains/lesson5/homework/CalcRunnable.java
@@ -1,4 +1,24 @@
 package ru.geekbrains.lesson5.homework;
 
-public class CalcRunnable {
-}
+public class CalcRunnable  implements Runnable {
+        private static float [] arr;
+        private static int cnt;
+        private static int offset;
+
+    public static void calcRun (float[] arr, int cnt, int offset) {
+        for (int i = offset; i < cnt; i++){
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+        public CalcRunnable (float [] arr, int cnt, int offset){
+            this.arr = arr;
+            this.cnt = cnt;
+            this.offset = offset;
+        }
+
+        @Override
+        public void run() {
+            calcRun(arr,cnt,offset);
+        }
+    }
+
--- src/ru/geekbrains/lesson5/homework/CalcThread.java
+++ src/ru/geekbrains/lesson5/homework/CalcThread.java
@@ -1,18 +1,18 @@
 package ru.geekbrains.lesson5.homework;
 
 
-public class NewThread extends Thread{
+public class CalcThread extends Thread{
     private float [] arr;
     private int cnt;
     private int offset;
 
-    public static void calcThread (float[] arr, int cnt, int offset) {
+    public static void calcRun (float[] arr, int cnt, int offset) {
         for (int i = offset; i < cnt; i++){
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
     }
 
-    public NewThread (float [] arr, int cnt, int offset){
+    public CalcThread (float [] arr, int cnt, int offset){
         this.arr = arr;
         this.cnt = cnt;
         this.offset = offset;
@@ -20,8 +20,7 @@ public class NewThread extends Thread{
 
     @Override
     public void run() {
-        calcThread(arr,cnt,offset);
-        System.out.println("new thread: cnt " + cnt + " off " + offset);
+        calcRun(arr,cnt,offset);
     }
 }
 
